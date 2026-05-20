89d7220989ee38be854cd4fa714c34f9cfbc3d97
nabilaalfira127-dev <nabilaalfira127@gmail.com>
2026-05-20 23:49:01 +0700

    tambah fitur data mobil dan histori

--- project,algo.cpp
+++ project,algo.cpp
@@ -17,3 +17,38 @@ void cls() {
 #endif
 
 }
+
+// ======================================================
+// SINGLE LINKED LIST - DATA MOBIL
+// ======================================================
+struct Mobil {
+
+    int id;
+    string nama;
+    string plat;
+    int tahun;
+    double harga;
+    string status;
+    int jumlahRental;
+
+    Mobil* next;
+};
+
+Mobil* headMobil = NULL;
+
+// ======================================================
+// DOUBLE LINKED LIST - HISTORI
+// ======================================================
+struct Histori {
+
+    string customer;
+    string mobil;
+    int lama;
+    double total;
+
+    Histori* prev;
+    Histori* next;
+};
+
+Histori* headHistori = NULL;
+Histori* tailHistori = NULL;
