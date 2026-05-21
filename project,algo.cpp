069837bc84eb6a21d0957216f0a72ea0584f71e7
rasyafirdian <rasyafirdian72@gmail.com>
2026-05-20 23:09:07 -0700

    Tampilan cls

---
+++ project,algo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+// ======================================================
+// CLEAR SCREEN
+// ======================================================
+void cls() {
+
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+
+}
